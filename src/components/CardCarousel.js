1d6b25a7637bada7024ce0aeb820fa48a6f6f47d
Jacob Douglas <chidorimachine@pop-os.localdomain>
2022-10-16 21:08:05 -0500

    Finally completed infinte card carousel

--- src/components/CardCarousel.js
+++ src/components/CardCarousel.js
@@ -1,27 +1,47 @@
 //import ProjectCard from "./ProjectCard";
+import { useEffect } from "react"
 import {React, useState} from "react"
 import { IconContext } from "react-icons"
 import {IoMdArrowDropleft, IoMdArrowDropright} from "react-icons/io"
 
 const CardCarousel = ({projectCards}) => {
     
-    const [currentProjectCard, setCurrentProjectCard] = useState(0)
+    const [cardIndex, setCardIndex] = useState(1)
+    const [cardList, setCardList] = useState([projectCards[projectCards.length-1], ...projectCards, projectCards[0]])
+    const [transitionEnabled, setTransitionEnabled] = useState(true)
+    const [leftButtonEnabled, setLeftButtonEnabled] = useState(true)
+    const [rightButtonEnabled, setRightButtonEnabled] = useState(true)
+    
+    useEffect(() => {
+        if(cardIndex === 1 || cardIndex === projectCards.length) {
+            setTransitionEnabled(true)
+        }
+    }, [cardIndex])
 
-    const prevProjectCard = () => {
-        if (currentProjectCard < 1) {
-            setCurrentProjectCard(projectCards.length - 1)
+    const handleTransitionEffect = () => {
+        if(cardIndex === 0) {
+            setTransitionEnabled(false)
+            setCardIndex(cardList.length-2)
+        } else if(cardIndex === cardList.length-1) {
+            setTransitionEnabled(false)
+            setCardIndex(1)
+        } else {
+            setTransitionEnabled(true)
         }
-        else {
-            setCurrentProjectCard(currentProjectCard - 1)
-        }
+        setRightButtonEnabled(true)
+        setLeftButtonEnabled(true)
     }
 
-    const nextProjectCard = () => {
-        if (currentProjectCard >= projectCards.length-1) {
-            setCurrentProjectCard(0)
+    const prevProjectCard = () => {
+        if(leftButtonEnabled) {
+            setCardIndex(cardIndex - 1)
+            setLeftButtonEnabled(false)
         }
-        else {
-            setCurrentProjectCard(currentProjectCard + 1)
+    }
+    const nextProjectCard = () => {
+        if(rightButtonEnabled) {
+            setCardIndex(cardIndex + 1)
+            setRightButtonEnabled(false)
         }
     }
     return (
@@ -33,8 +53,8 @@ const CardCarousel = ({projectCards}) => {
                     </div>
                 </IconContext.Provider>
                 <div className="overflow-x-scroll w-96 h-[45rem] scrollbar-hide">
-                    <div className={`flex transition-all ease-linear duration-500`} style={{transform : `translateX(-${currentProjectCard*100}%)`}}>
-                        {projectCards}
+                    <div className={`flex ${transitionEnabled ? "transition-all" : "transition-none"} ease-linear duration-500`} style={{transform : `translateX(-${cardIndex*100}%)`}} onTransitionEnd={handleTransitionEffect}> 
+                        {cardList}
                     </div>
                 </div>
                 <IconContext.Provider value={{size: "1.75em"}}>
