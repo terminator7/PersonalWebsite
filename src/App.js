e6978efa02b363c14ac5b7c65f7545598208816c
Jacob Douglas <chidorimachine@pop-os.localdomain>
2022-10-22 21:48:35 -0500

    Created and added Passions section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './styles/App.css'
 import { NavBar } from './components/Nav';
-import { Greetings, HomePage } from './pages/Greetings';
+import { Greetings } from './pages/Greetings';
+import Passions from './pages/Passions';
 import { AboutPage } from './pages/About';
 import ProjectPage from './pages/Project'
 import { ContactPage} from './pages/Contact'
@@ -13,6 +14,8 @@ function App() {
         <NavBar></NavBar>
       </header>
       <Greetings></Greetings>
+      <div className="p-3"></div>
+      <Passions></Passions>
     </div>
   );
 }
---
+++ src/pages/Passions.js
@@ -0,0 +1,23 @@
+
+
+import PassionCard from "../components/PassionCard"
+import { HiOutlineDesktopComputer } from 'react-icons/hi'
+import { AiOutlineHtml5 } from 'react-icons/ai'
+import {IoGameControllerOutline} from 'react-icons/io5'
+const Passions = (props) => {
+    return (
+        <section>
+            <h2 className="text-gray-200 text-center font-medium text-[3rem]">My Passions</h2>
+            <div className="py-6"></div>
+            <div className="passionsCardContainer flex justify-center gap-10">
+                <PassionCard title="Game Design" icon={<IoGameControllerOutline></IoGameControllerOutline>} iconSize="8rem" iconColor="text-blue-500"></PassionCard>
+                <PassionCard title="Full Stack Dev" icon={<AiOutlineHtml5></AiOutlineHtml5>} iconSize="8rem" iconColor="text-purple-400"></PassionCard>
+                <PassionCard title="Machine Learning" icon={<HiOutlineDesktopComputer></HiOutlineDesktopComputer>} iconSize="8rem" iconColor="text-blue-500"></PassionCard>
+            </div>
+            <div className="py-3"></div>
+            <div className="text-gray-200 px-20 text-center text-xl">All these technologies are something I find extremely interesting. Have really only taken a full dive into full stack devlopment but I am excited to start the journey into Machine Learning and Game Devlopment</div>
+        </section>
+    )
+}
+
+export default Passions
