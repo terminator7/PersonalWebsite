4c49e16aff5b6c1cc78a614595c92d2a2322d620
Jacob Douglas <chidorimachine@pop-os.localdomain>
2022-10-22 22:46:32 -0500

    Added About me section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './styles/App.css'
 import { NavBar } from './components/Nav';
 import { Greetings } from './pages/Greetings';
 import Passions from './pages/Passions';
-import { AboutPage } from './pages/About';
+import AboutMe from './pages/About';
 import ProjectPage from './pages/Project'
 import { ContactPage} from './pages/Contact'
 
@@ -16,6 +16,8 @@ function App() {
       <Greetings></Greetings>
       <div className="p-3"></div>
       <Passions></Passions>
+      <div className="p-10"></div>
+      <AboutMe></AboutMe>
     </div>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,23 @@
+import '../styles/App.css'
+import profilePicture from '../images/pictureofme.jpg'
 
-
-export let AboutPage = (props) => {
+const AboutMe = (props) => {
     return (
-        <div className="mx-auto pt-24 h-screen from-blue-800 to-purple-700 bg-gradient-to-r">
-            <div className="bg-white xs:w-full md:w-3/4 py-10 mx-auto rounded-lg">
-                <h1 className="text-7xl font-bold text-purple-700 text-center">About Me</h1>
-                <div className="border-4 border-blue-500 mt-4 mx-5 p-2 rounded-md text-xl">
+        <section>
+            <h2 className="text-gray-200 text-center font-medium text-[3rem]">About Me</h2>
+            <div className="py-6"></div>
+            <div className="greetingSection text-gray-200 flex mx-5 border rounded-md">
+                <div className="imageContainer w-1/2 p-10 flex justify-center items-center">
+                    <div className="imgHolder border-2 rounded-[50%] h-3/4 w-3/4">
+                        <img className="w-full h-full object-cover rounded-[50%]" src={profilePicture} alt="Picture of Jacob Douglas" />
+                    </div>
+                </div>
+                <div className="w-1/2 flex items-center px-6 text-xl">
                     <p>Started programming when I was in 7th grade. It all started when I wanted to make my own mods for my favorite game at the time, Minecraft. Ever since then, its just been an exponential curve of always wanting to learn more. It was then Java, Python and even Lua by the time I had reached High school. Since then, I have been focusing on more complex things that interest me such as game development, smart home technology, and front end and back end web development. I hope after my current semester, when I got done studying computer security and networking, I hope to move on to even more complex things such as machine learning algorithms!</p>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
+
+export default AboutMe
